cfb084c478b95cee573a059ad4bcf7d75e22247e
Ariel <arielr.suarez@gmail.com>
2020-07-11 20:19:58 -0300

    Lentti back end finished

--- Aplicaciones/IntroNode/serverless/api/index.js
+++ Aplicaciones/IntroNode/serverless/api/index.js
@@ -1,22 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const meals = require('./routes/meals')
+const orders = require('./routes/orders')
 const app = express()
+app.use(bodyParser.json())
+app.use(cors())
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
 
-const Users = mongoose.model('User', new mongoose.Schema({ name: String }))
-
-Users.create({ name: 'Andy Hull'})
-Users.create({ name: 'Franco Orellana'})
-Users.create({ name: 'Mercedes'})
-Users.create({ name: 'Ari'})
-Users.create({ name: 'Pablo'})
-Users.create({ name: 'Fede MOreno'})
-
-app.get('*', (req, res) => {
-    Users.find()
-    .then(x => res.send(x))
-})
+app.use('/api/meals', meals)
+app.use('/api/orders', orders)
 
 module.exports = app
 
